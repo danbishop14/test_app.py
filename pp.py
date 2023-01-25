1850b6016e173cf90ec17d129581d33d43f8afac
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-24 22:04:46 -0700

    Update pp.py

--- pp.py
+++ pp.py
@@ -4,19 +4,18 @@ URL = "https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_track
 
 def split_df_to_parquet(df, output_dir):
     # Group the DataFrame by state_code
+    df = df.loc[df['property_type'] == 'Single Family Residential'].assign(period_end = pd.to_datetime(df['period_end']),zipcode = df['region'].str.replace('Zip Code:',''))
     grouped = df.groupby('state_code')
 
     # Iterate over each group and write to a separate Parquet file
     for state_code, group in grouped:
         group.to_parquet(f'{output_dir}/{state_code}.parquet')
-        group.to_csv(f'{output_dir}/{state_code}.csv')
+        # group.to_csv(f'{output_dir}/{state_code}.csv')
 
 # Example usage
-df = pd.read_csv('URL',sep = '\t')
+df = pd.read_csv(URL,sep = '\t')
 
-# df = df.head()
+
 # df.to_csv('test.csv')
 
-print(df.head())
-print(df.columns)
 split_df_to_parquet(df, 'output_parquet_files')
